be127a80bada7c88d119bd24eca340afabdc86b3
Derrick Lui <derricklui1@gmail.com>
2015-02-25 09:27:19 -0500

    mergesort started

---
+++ 04-mergesort/mergesort.java
@@ -0,0 +1,13 @@
+public class mergesort{
+    public ArrayList<Integer> merge(ArrayList<Integer> a, ArrayList<Integer> b){
+	while (a.size() <=1 || b.size() <=1){
+	    if (a.get(0) <= b.get(0)){
+		
+	    }
+
+	}
+	    
+
+    }
+
+}
