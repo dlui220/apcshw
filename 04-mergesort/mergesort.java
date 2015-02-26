92710816f5bcb8a145e6c12ab6a5dc1574bbbeb2
Derrick Lui <derricklui1@gmail.com>
2015-02-26 09:22:17 -0500

    adding merge with arrays

--- 04-mergesort/mergesort.java
+++ 04-mergesort/mergesort.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class mergesort{
 
@@ -19,9 +20,48 @@ public class mergesort{
 	return result;
     }
 
+    public int[] merge(int[] a, int[] b){
+	int[] result = new int[a.length + b.length];
+	int ac = 0;
+	int bc = 0;
+	int rc = 0;
+	while (bc<b.length || ac<a.length){
+	    if (ac == a.length){
+		result[rc] = b[bc];
+		rc++;
+		bc++;
+	    } else if (bc == b.length){
+		result[rc] = a[ac];
+		rc++;
+		bc++;
+	    } else if ((ac < a.length && bc < b.length)&&(a[ac] > b[bc])){
+		result[rc] = a[ac];
+		rc++;
+		ac++;
+	    } else if ((ac < a.length && bc < b.length)&&(a[ac] < b[bc])){
+		result[rc] = b[bc];
+		rc++;
+		bc++;
+	    }
+		
+	}
+	return result;
+    }
+
+    /*
+      public ArrayList<Integer> msort(ArrayList<Integer> L){
+	int m = L.size()/2;
+	ArrayList<Integer> a = new ArrayList<Integer>();
+	ArrayList<Integer> b = new ArrayList<Integer>();
+	if (L.size() >= 1){
+	    a = L.addAll(
+	    
+	    
+    }
+    */
     public static void main(String[] args){
 	mergesort m = new mergesort();
-	
+	/*
 	ArrayList<Integer> first = new ArrayList<Integer>();
 	ArrayList<Integer> second = new ArrayList<Integer>();
 
@@ -37,8 +77,24 @@ public class mergesort{
 
 	System.out.println(first);
 	System.out.println(second);
+	*/
+	int[] firstarray = new int[4];
+	int[] secarray = new int[4];
+	firstarray[0] = 1;
+	firstarray[1] = 6;
+	firstarray[2] = 8;
+	firstarray[3] = 10;
 
-	System.out.println(m.merge(first,second));
+	secarray[0] = 2;
+	secarray[1] = 3;
+	secarray[2] = 7;
+	secarray[3] = 9;
 	
+	System.out.println(Arrays.toString(firstarray));
+	System.out.println(Arrays.toString(secarray));
+
+	//	System.out.println(m.merge(first,second));
+	
+	System.out.println(Arrays.toString(m.merge(firstarray,secarray)));
     }
 }
