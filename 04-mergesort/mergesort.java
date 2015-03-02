a18efcdc730949c69383f1e9446d4f683ecb136e
Derrick Lui <derricklui1@gmail.com>
2015-03-02 02:16:49 -0500

    updated

--- 04-mergesort/mergesort.java
+++ 04-mergesort/mergesort.java
@@ -20,7 +20,7 @@ public class mergesort{
 	return result;
     }
 
-    public int[] merge(int[] a, int[] b){
+    /*   public int[] merge(int[] a, int[] b){
 	int[] result = new int[a.length + b.length];
 	int ac = 0;
 	int bc = 0;
@@ -46,41 +46,60 @@ public class mergesort{
 		bc++;
 	    }
 	    */
-		
+    /*	
 	}
 	return result;
     }
-
-    /*
-      public ArrayList<Integer> msort(ArrayList<Integer> L){
-	int m = L.size()/2;
-	ArrayList<Integer> a = new ArrayList<Integer>();
-	ArrayList<Integer> b = new ArrayList<Integer>();
-	if (L.size() >= 1){
-	    a = L.addAll(
-	    
-	    
-    }
     */
+
+      public ArrayList<Integer> msort(ArrayList<Integer> L){
+	  ArrayList<Integer> result = new ArrayList<Integer>();
+	  int m = L.size()/2;
+	  if (L.size() >= 1){
+	      ArrayList<Integer> a = new ArrayList<Integer>(L.subList(0,m));
+	      ArrayList<Integer> b = new ArrayList<Integer>(L.subList(m,L.size()));
+	      merge(msort(a),msort(b));
+	}
+	  return result;
+      }
+
+      
+	
+
+			 
+	    
+	    
+
+    
     public static void main(String[] args){
 	mergesort m = new mergesort();
-	/*
+	
 	ArrayList<Integer> first = new ArrayList<Integer>();
 	ArrayList<Integer> second = new ArrayList<Integer>();
 
-	first.add(1);
-	first.add(6);
 	first.add(8);
-	first.add(10);
+	first.add(6);
+	first.add(1);
+	first.add(5);
 
-	second.add(2);
 	second.add(3);
 	second.add(7);
+	second.add(2);
 	second.add(9);
 
 	System.out.println(first);
 	System.out.println(second);
-	*/
+
+	ArrayList<Integer> testcase = new ArrayList<Integer>();
+	testcase.add(1);
+	testcase.add(9);
+	testcase.add(2);
+	testcase.add(6);
+	testcase.add(8);
+	testcase.add(7);
+	testcase.add(3);
+	
+	/*
 	int[] firstarray = new int[4];
 	int[] secarray = new int[4];
 	firstarray[0] = 1;
@@ -95,9 +114,10 @@ public class mergesort{
 	
 	System.out.println(Arrays.toString(firstarray));
 	System.out.println(Arrays.toString(secarray));
+	*/
 
 	//	System.out.println(m.merge(first,second));
 	
-	System.out.println(Arrays.toString(m.merge(firstarray,secarray)));
+	System.out.println(m.msort(testcase));
     }
 }
